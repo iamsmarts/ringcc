9106bbc8d3515792874785e97bdac98ae548c975
iamsmarts <iamsmarts@Rudys-MacBook-Air.local>
2019-02-19 16:06:11 -0800

    code after 30 min or so

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// https://www.ring.com/form-submit.php
+
 
 class App extends Component {
+state = {
+  firstname:'',
+  lastname:'',
+  address:'',
+  phone:null,
+  email:''
+}
+handleChange = (e) => {
+  console.log(e.target.name, e.target.value);
+  this.setState({[e.target.name]: e.target.value})
+}
+handleSubmit = (e) =>{
+  e.preventDefault();
+  console.log(this.state);
+  // const subData = {...this.state}
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <form onSubmit={this.handleSubmit}>
+        <label>First Name:</label>
+        <input type="text" value={this.state.firstname} name="firstname" onChange={this.handleChange}/>
+
+        <label>Last Name:</label>
+        <input type="text" value={this.state.lasttname} name="lastname" onChange={this.handleChange}/>
+
+        <label>Address:</label>
+        <input type="text" value={this.state.address} name="address" onChange={this.handleChange}/>
+
+        <label>Phone Number:</label>
+        <input type="tel" value={this.state.firstname} name="firstname" onChange={this.handleChange}/>
+
+        <label>Email:</label>
+        <input type="email" value={this.state.firstname} name="firstname" onChange={this.handleChange} />
+
+        <input type="submit" value="Submit" />
+      </form>
+
       </div>
     );
   }
